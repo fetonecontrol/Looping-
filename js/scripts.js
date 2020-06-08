66b73372385722b1eb9a5218262fc7a082543d59
Ian Gregg <iangregg188@gmail.com>
2020-06-08 14:13:29 -0700

    Created a loop alerting the user of our favorite friends.
    Co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+  const arrayOfPeopleILike = ["ian", "Fred", "alex", "chris"];
+  arrayOfPeopleILike.forEach(function(peopleILike){
+    alert('These are our friends:' + peopleILike);
+  });
 });
+
+
+//let thingsILike = "I like...";
+//const arrayOfThingsILike = ["bubble baths", "kittens", "good books", "clean code"];
+//arrayOfThingsILike.forEach(function(thingILike) {
+//  thingsILike = thingsILike.concat(" " + thingILike + "!");
+//});
+//thingsILike;
